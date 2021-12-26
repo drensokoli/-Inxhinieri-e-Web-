d6222704a62678a274fde649c0da19e8a9d9449f
Dren Sokoli <drensokoli@gmail.com>
2021-12-26 12:05:16 +0100

    Validimi i username dhe password
    Kemi bere validimin e perdoruesit dhe fjalekalimit duke i ruajtur te dhenat ne local storage

--- index.js
+++ index.js
@@ -23,8 +23,31 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
+
+  console.log("slides:")
+  console.log(slides)
+  console.log("slideIndex:")
+  console.log(slideIndex)
+  console.log("slideIndex-1:")
+  console.log(slideIndex-1)
+  console.log("slides[slideIndex-1]:")
+  console.log(slides[slideIndex-1])
+
+  console.log("slides:")
+  console.log(slides)
+  console.log(slides.item(0))
+  console.log(slides.item(1))
+  console.log(Array.from(slides)[2])
+  console.log(Array.from(slides))
+
+  console.log("slides:")
+  console.log(slides)
+  console.log(typeof(slides))
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+
 }
 
 
@@ -43,14 +66,14 @@ function store(){
     
     if(password.value.length<8){
         alert('Fjalëkalimi duhet të ketë së paku 8 karaktere');
-    }else if(!password.value.match(numbers)){
+    }else if(!password.value.match(/[0-9]/)){
         alert('Fjalëkalimi duhet të përmbajë së paku një numër');
-    }else if(!password.value.match(upperCaseLetters)){
+    }else if(!password.value.match(/[A-Z]/)){
         alert('Fjalëkalimi duhet të përmbajë së paku një shkronjë të madhe');
     }else if(!password.value == pw_confirm.value){
         alert('Fjalëkalimi që keni shkruar nuk përputhet')
     }else{
-        localStorage.setItem('emri', emri.value);
+        localStorage.setItem('perdoruesi', perdoruesi.value);
         localStorage.setItem('password', password.value);
         alert('Jeni regjistruar');
     }
@@ -62,11 +85,17 @@ function check(){
 
     var PERDORUESI = document.getElementById('PERDORUESI');
     var PASSWORD = document.getElementById('PASSWORD');
-    var userRemember = document.getElementById("rememberMe");
+    // var userRemember = document.getElementById("rememberMe");
 
     if(PERDORUESI.value == storedName && PASSWORD.value == storedPw){
+        localStorage.setItem('loggedInUser', perdoruesi)
+        document.getElementById("login_btn").onclick = function () {
+            location.href = "index.html";
+        };
         alert('You are logged in.');
+        
     }else{
-        alert('Error on login');
+        alert('Emri i perdoruesit ose fjalëkalimi janë gabim.');
     }
 }
+
